510579985e65f7f8fb929ed2599bec5b39eb997b
KhaledRezk <khaledkra2112@gmail.com>
2020-06-27 05:18:12 +0200

    add sparse table template to library

---
+++ Libraries/sparse_table.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define INF 1000'000'000
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+template<typename T>
+struct sparse_table{
+	int n;
+	vector < vector < T > > table;
+	T (*combine) (T,T);
+	
+	sparse_table(int N, vector < T > & v, T (*comb) (T,T)){
+		n = N, combine = comb;
+		table.assign(__lg(n)+1, vector < T > (n));
+		build_sparse_table(v);
+	}
+	void build_sparse_table(const vector < T > & v){
+			
+		for(int i = 0; i < n; i++)// for 0 based array
+			table[0][i] = v[i];
+		
+		int mx_row = __lg(n);
+		for(int row = 1; row <= mx_row; row++){
+			int c_range = 1 << row;
+			for(int col = 0; col + c_range - 1 < n; col++){
+				table[row][col] = combine(table[row-1][col] , table[row-1][col + (c_range >> 1)]);
+			}
+		}
+	}
+	
+	T query_o1(int l, int r){// overlap freindly
+		if(l > r)
+			swap(l,r);
+		int msb = __lg(r - l + 1), s_range = 1 << msb;
+		return combine(table[msb][l], table[msb][r - (s_range - 1)]);
+	}
+	
+	T query_ologn(int l, int r){// non overlap friendly functions as +, * ..
+		if(l > r)
+			swap(l,r);
+		T ans;
+		bool started = 0;// not to add initall value add 
+		while(l <= r){
+			int msb = __lg(r - l + 1);
+			auto nxt = table[msb][l]; l += 1 << msb;
+			if(started)
+				ans = combine(ans, nxt);
+			else
+				ans = nxt, started = true;
+		}
+		return ans;
+	}
+};
+
